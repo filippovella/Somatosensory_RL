0881277cbe24f43ae708b2db22a8ac804c45d6d9
filippovella <33904265+filippovella@users.noreply.github.com>
2023-01-18 18:20:35 +0100

    Add files via upload

--- Main.m
+++ Main.m
@@ -5,22 +5,26 @@ function Main
     addpath('./Somatosensory/','-end')
     addpath('./libs/','-end')
     
-    curr_d = pwd
+    curr_d = pwd;
     
     list = {'Energy Modes','Energy Modes with an urge','Current Drive actions'};
     set(0, 'DefaultUICOntrolFontSize', 16)
-    [indx,tf] = listdlg('ListString',list,'SelectionMode','single', 'ListSize', [300 100], 'PromptString', 'RL Somatosensory','uh',50);
-   
-    
-    if(indx ==1)
-        disp(['Energy Modes']);
-        RL_Energy_Modes(curr_d);
-    elseif(indx ==2)
-        disp(['Energy Modes with a urge']);
-        RL_EnergyModes_URGE(curr_d);
-    else
-        disp(['Current Drive actions']);
-        RL_CurrentPain(curr_d);
+    tf = 1;
+    while (tf==1)
+        [indx,tf] = listdlg('ListString',list,'SelectionMode','single', 'ListSize', [300 100], 'PromptString', 'RL Somatosensory','uh',50);
+
+        if(tf==1)
+            if(indx ==1)
+                disp(['Energy Modes']);
+                RL_Energy_Modes(curr_d);
+            elseif(indx ==2)
+                disp(['Energy Modes with a urge']);
+                RL_EnergyModes_URGE(curr_d);
+            else
+                disp(['Current Drive actions']);
+                RL_CurrentPain(curr_d);
+            end
+        end
     end
     
     rmpath('./Energy_ActionModes/');
